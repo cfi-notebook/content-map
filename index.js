4de721eaacb8364ae5400325bc12b725c7bb7fd9
Ryan Burnette <ryan.burnette@gmail.com>
2019-03-28 12:35:00 -0400

    create a library

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ var ContentMap = {};
 
 ContentMap.create = require('./lib/create');
 ContentMap.createFromYaml = require('./lib/create-from-yaml');
+ContentMap.createAll = require('./lib/create-all');
 
 module.exports = ContentMap;
